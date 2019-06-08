4151934f29933a7ac9e86defdee612e693728332
Soumyadeep <soumo.sinha21@gmail.com>
2019-06-08 13:41:22 +0530

    Update loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,5 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
