252ba61eb10cf6568bc8238c0ea35e8b6c3e760f
Soumyadeep <soumo.sinha21@gmail.com>
2019-06-08 18:31:33 +0530

    Update city_screen.dart

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -33,7 +33,7 @@ class _CityScreenState extends State<CityScreen> {
               ),
               Container(
                 padding: EdgeInsets.all(20.0),
-                child: null,
+                child: TextField(),
               ),
               FlatButton(
                 onPressed: () {},
