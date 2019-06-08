d2c1ea49e685ed983713850e137f9a2368d8b6bf
Soumyadeep <soumo.sinha21@gmail.com>
2019-06-08 14:35:38 +0530

    Update loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -20,8 +20,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print('Longitude : ${location.longitude}  Latitude : ${location.latitude}');
   }
 
-  void getData() async{
-    Response response = await get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+  void getData() async {
+    http.Response response = await http.get(
+      'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22',
+    );
     print(response.body);
   }
 
