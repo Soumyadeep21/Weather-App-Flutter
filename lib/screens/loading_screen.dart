8f240021f01959c97fbf0fcccd19698713ae2678
Soumyadeep <soumo.sinha21@gmail.com>
2019-06-08 16:29:49 +0530

    Update loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
+import 'package:clima/screens/location_screen.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = '17c4e421f91f1b18b26186f87330a6b7';
 
@@ -28,11 +30,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     NetworkHelper networkHelper = NetworkHelper(
       'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'
     );
-    var weatherData = await networkHelper.getData();    
+    var weatherData = await networkHelper.getData();
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (_) => LocationScreen()
+    ));    
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
